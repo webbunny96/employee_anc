d529126212147749e5aaf5cd7add49b5057575cc
webbunny96 <Web.bunny96@gmail.com>
2024-05-28 20:23:30 +0300

    Create model and migrate

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    hire_date = models.DateField()
+    email = models.EmailField(unique=True)
+    manager = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subordinates')
+
+    def __str__(self):
+        return self.full_name
